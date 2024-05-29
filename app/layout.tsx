f97b7c88b1277ba5e6fb92b20ec9116300951538
zxSage <zssSage@proton.me>
2024-05-29 18:15:44 +0200

    refactord page org

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,23 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { StackedLayout } from '@/components/stacked-layout'
+import { Navbar, NavbarItem, NavbarLabel, NavbarSection, NavbarDivider, NavbarSpacer } from '@/components/navbar'
+import { Sidebar, SidebarBody, SidebarHeader, SidebarItem, SidebarLabel, SidebarSection } from '@/components/sidebar'
+
+import { SparklesIcon } from '@heroicons/react/20/solid'
+
+const navItems = [
+  { label: 'Live Arena', url: '/' },
+  { label: 'Leaderboard', url: '/events' },
+  { label: 'Order Explorer', url: '/orders' },
+  { label: 'Stats', url: '/stats' },
+]
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Arena",
+  title: "Urani Arena",
   description: "Watch sophisticated players eat glass live.",
 };
 
@@ -16,7 +29,54 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StackedLayout
+        navbar={
+          <Navbar>
+              <NavbarItem className="max-lg:hidden" href="https://urani.xyz">
+                <img src="/urani.svg" />
+                <NavbarLabel>Urani</NavbarLabel>
+              </NavbarItem>
+            <NavbarDivider className="max-lg:hidden mr-10" />
+            <NavbarSection className="max-lg:hidden">
+              {navItems.map(({ label, url }) => (
+                <NavbarItem key={label} href={url}>
+                  {label}
+                </NavbarItem>
+              ))}
+            </NavbarSection>
+            <NavbarSpacer />
+            <NavbarSection>
+              <NavbarItem>
+                <SparklesIcon />
+                2HPwR...5gSJr
+              </NavbarItem>
+            </NavbarSection>
+          </Navbar>
+        }
+        sidebar={
+          <Sidebar>
+            <SidebarHeader>
+              <NavbarItem href="https://urani.xyz">
+              <img src="/urani.svg" className="w-20 h-20 inline mr-5" />
+              <span className="text-2xl font-bold">Urani</span>
+              </NavbarItem>
+            </SidebarHeader>
+            <SidebarBody>
+              <SidebarSection>
+                {navItems.map(({ label, url }) => (
+                  <SidebarItem key={label} href={url}>
+                    {label}
+                  </SidebarItem>
+                ))}
+              </SidebarSection>
+            </SidebarBody>
+          </Sidebar>
+        }
+      >
+        {children}
+      </StackedLayout>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,6 @@
-import { Input } from './components/input'
-import { Field, FieldGroup, Label } from './components/fieldset'
-import { Button } from './components/button'
+
 import Arena from './arena'
 
-import { TokenTransferGraph } from '@/components/token-transfer-graph/token-transfer-graph'
-
-
 export default function Home() {
-  return <Arena>
-    <TokenTransferGraph />
-  </Arena>
+  return <Arena/>
 }
