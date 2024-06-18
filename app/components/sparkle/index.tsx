9304ec94f2b152d4dd2ddfcc0ba713653f0783f5
zxSage <zssSage@proton.me>
2024-06-17 22:53:44 -0700

    fix build err

--- app/components/sparkle/index.tsx
+++ app/components/sparkle/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 
-const Sparkle = ({ children, count = 20 }) => {
+const Sparkle = ({ children, count = 20 }: { children:React.ReactNode, count:number}) => {
   return (
     <div className="sparkle-container">
       {children}
